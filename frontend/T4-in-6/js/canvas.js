3b1970207ab85df225ae4224130017708d3fb700
sergio <sergio@10.13.35.242>
2016-08-05 16:50:35 -0500

    Stable inifinite scroll of the "Numbers" card elements.

--- frontend/T4-in-6/js/canvas.js
+++ frontend/T4-in-6/js/canvas.js
@@ -28,9 +28,31 @@
      */
     var Canvas = function (ele, data) {
         //CONSTANTS --->
-        this.WIDTH = 200;
-        this.MARGIN = 5;
+        /** NUMBER CARD WIDTH */
+        this.WIDTH = 300;
+        /** MARGIN WIDTH */
+        this.MARGIN = 10;
+        
+        /**
+         * Determines how many extra linear elements must be
+         * placed in order to cover all canvas area.
+         * @type {number}
+         * @protected
+         */
+        this.extras = 0;
 
+        /**
+         * Real Card number distance
+         * @type {number}
+         * @protected
+         */
+        this.distance = this.WIDTH + (this.MARGIN * 2);
+        /**
+         * Class with the transformation ease attributes
+         * for the slowing effect.
+         * 
+         */
+        this.easeClass = "transform-numbers-ease";
         /**
          * DOM Element for the canvas.
          * @type {DOMElement}
@@ -44,6 +66,12 @@
          */
         this.numbers = data;
         /**
+         * Last successful value set to the x transformation
+         * @protected
+         * @type {number}
+         */
+        this.lastX = 0;
+        /**
          * jQuery object from the DOMElement
          * @type {jQuery}
          * @protected
@@ -56,11 +84,37 @@
          */
         this.numbersRow = this.jQueryElement.parent();
         /**
+         * Amount of pan made to the left
+         * @type {number}
+         * @protected
+         */
+        this.panned = 0;
+        /**
+         * Current index of the ncard to be loaded on the left side of the tile.
+         * @type {number}
+         * @protected
+         */
+        this.leftIndex = 0;
+        /**
+         * Current index of the ncard to be loaded on the right side of the tile.
+         * @type {number}
+         * @protected
+         */
+        this.rightIndex = 0;
+        /**
          * Pixel location of the scroll.
          * @type {number}
          * @private
          */
         this.scrollLocation = 0;
+        /**
+         * Array of CNumber objects currently present on the physical
+         * view of the wheel.
+         * @protected
+         * @type {Array.<CNumber>}
+         */
+        this.wheel = [];
+
 
 
         //CALL TO PROTO METHOD init.
@@ -75,40 +129,333 @@
          * @returns {undefined}
          */
         init: function () {
-            var n = this.numbers;
             var me = this;
-            
-            for (var i in n) {
-                var num = new CNumber(n[i], this.WIDTH, this.MARGIN);
-                this.jQueryElement.append(num.jquery());
-            }
-            this.jQueryElement.css("width", (n.length * (this.WIDTH + (this.MARGIN * 2))) + "px");
 
-            //Endless scroll
-            this.jQueryElement.endless({
-                direction: 'horizontal',
-                scrollbar: 'disable'
-            });
-            
+            this.loadNumberCards();
+
             //HAMMER EVENTS - PAN
             this.ham = new Hammer(this.element.parentNode);
             //Hammer events
-            this.ham.on('panstart', function(evt){
-                me.scrollLocation = me.numbersRow.scrollLeft();
+            this.ham.on('panend', function (evt) {
+                me.onPanEnd(evt);
             });
-            this.ham.on('panmove', function(evt){
-                var distance = me.scrollLocation - evt.deltaX;
-                
-                me.numbersRow.scrollLeft(distance);
-                //e.preventDefault();
+            this.ham.on('panstart', function (evt) {
+                me.onPanStart(evt);
             });
-            
-            
+            this.ham.on('panmove', function (evt) {
+                me.onPan(evt);
+            });
+
+
             //PAN CONFIGURATIONS
             this.ham.get('pan').set({threshold: 0, direction: Hammer.DIRECTION_HORIZONTAL});
-           
+
+        },
+        /**
+         * Gets the number of extra cards that should be loaded to the left and to the right
+         * in orther to cover all screen/canvas available space.
+         * @private
+         * @returns {number}
+         */
+        calculateExtras:function(){
+            var w = this.element.offsetWidth;
+            var cd = this.distance;
+            var shouldbe = Math.floor(w/cd)+2;
+            var thereare = this.numbers.length;
+            //ONLY IF NEEDED EXTRAS -->
+            if(thereare < shouldbe){
+                var extrastotal = shouldbe-thereare;
+                //FROM EXTRA TOTAL GET ONLY EXTRA ON EACH SIDE -->
+                var realextra =  Math.ceil(extrastotal/2);
+                return realextra;
+                
+            }
+            //IF NO NEEDED (numbers HAS ENOUGH CARDS TO COVER SCREEN)-->
+            return 0;
+            
+            
+        },
+        /**
+         * Makes the initial card construction
+         * @private
+         * @returns {undefined}
+         */
+        loadNumberCards: function () {
+            this.extras = this.calculateExtras();
+
+            this.currentLeftIndex = (0 - this.extras);
+            this.currentRightIndex = (this.numbers.length + this.extras - 1);
+
             
 
+            //THE OTHER THAN FIRST AS LAST
+            var initial = this.currentLeftIndex + 1;
+            for (var i = initial; i <= this.currentRightIndex; i++) {
+                this.loadACard(i);
+            }
+            //FIRST ELEMENT REGISTERED AS FIRST
+            this.loadACard(this.currentLeftIndex, true);
+            //this.jQueryElement.css("width", (this.jQueryElement.children().length * this.distance) + "px");
+        },
+        /**
+         * Loads a card by index;
+         * @public
+         * @param {number} i Index from the "numbers" property.
+         * @param {boolean} [first] If present and true states that the loading should be as first element.
+         * @returns {undefined}
+         */
+        loadACard: function (i, first) {
+            var n = this.numbers;
+            var ri = this.realIndex(i);
+
+            var num = new CNumber(n[ri], this.WIDTH, this.MARGIN, i);
+            if (first) {
+                
+                this.registerFirstNumber(num);
+            } else {
+                
+                this.registerLastNumber(num);
+            }
+
+        },
+        /**
+         * Executes the instructions to move the view according to the
+         * pan gesture.
+         * @private
+         * @returns {undefined}
+         */
+        onPan: function (event) {
+            /** @param {CNumber} */
+            this.iterateWheel(function (cnumber) {
+                cnumber.pan(event);
+            });
+        },
+        /**
+         * Actions to be executed on pan start
+         * @param {HammerEvent} event
+         * @private
+         */
+        onPanStart: function (evt) {
+            /** @param {CNumber} */
+            this.iterateWheel(function (cnumber) {
+                cnumber.panStart();
+            });
+        },
+        /**
+         * Sets the final attribs according to the final pan position.
+         * @private
+         * @returns {undefined}
+         */
+        onPanEnd: function (evt) {
+            /** @param {CNumber} */
+            this.iterateWheel(function (cnumber) {
+                cnumber.panEnd(evt);
+            });
+
+
+            //this.panned += evt.distance;
+            //this.panned = this.evalPan(this.panned);
+
+
+        },
+        /**
+         * Registers a new CNumber object in the wheel at the end of it.
+         * @param {CNumber} cnumber CNumber object.
+         * @private
+         * @returns {undefined}
+         */
+        registerLastNumber: function (cnumber) {
+            var me = this;
+            this.jQueryElement.append(cnumber.jquery());
+            var i = this.wheel.push(cnumber) - 1;
+            var prev = i - 1;
+            var left = 0;
+            //this.jQueryElement.css("width", (this.jQueryElement.children().length * this.distance) + "px");
+
+            //PREVIOUS CARD NOW IGNORES THE BOUND REACHING EVENT
+            if (prev >= 0) {
+                var p = this.wheel[prev];
+                p.ignore();
+                left = p.getLeft() + this.distance;
+            }
+            //NEW CARD INITS AND ADDS THE BOUND REACHING EVENT.
+            cnumber.init(left);
+            
+            cnumber.listen(function (l) {
+                if (l < this.limit) {
+                    me.proceed(1);
+                }
+            });
+        },
+        /**
+         * Registers a new CNumber object in the wheel at the beginning of it.
+         * @param {CNumber} cnumber CNumber object.
+         * @private
+         * @returns {undefined}
+         */
+        registerFirstNumber: function (cnumber) {
+            var me = this;
+            
+            
+            this.jQueryElement.prepend(cnumber.jquery());
+            this.wheel.unshift(cnumber);
+            //this.jQueryElement.css("width", (this.jQueryElement.children().length * this.distance) + "px");
+            var prev = 1;
+            var left = 0;
+
+            //PREVIOUS CARD NOW IGNORES THE BOUND REACHING EVENT
+            if (prev < this.wheel.length) {
+                var p = this.wheel[prev];
+                p.ignore();
+                left = p.getLeft() - this.distance;
+            }
+            //NEW CARD INITS AND ADDS THE BOUND REACHING EVENT.
+            cnumber.init(left);
+            
+            cnumber.listen(function (l) {
+                
+                if (l > 0) {
+                    me.proceed(-1);
+                }
+            });
+        },
+        /**
+         * Un-Registers a new CNumber object in the wheel.
+         * @private
+         * @returns {undefined}
+         */
+        unregisterLastNumber: function () {
+            this.wheel.pop();
+
+            var me = this;
+            var n = this.wheel[this.wheel.length - 1];
+
+            n.listen(function (l) {
+                if (l < this.limit) {
+                    me.proceed(1);
+                }
+            });
+
+        },
+        /**
+         * Un-Registers a new CNumber object in the wheel.
+         * @private
+         * @returns {undefined}
+         */
+        unregisterFirstNumber: function () {
+            this.wheel.shift();
+
+            var me = this;
+            var n = this.wheel[0];
+
+            n.listen(function (l) {
+                if (l > 0) {
+                    me.proceed(-1);
+                }
+            });
+        },
+        /**
+         * Makes the jQuery/CNumber Card Adding/Removal procedures to the start/end
+         * of the reel depending on the direction.
+         * @private
+         * @param {number} dir Direction can be either 1 or -1
+         * @returns {undefined}
+         */
+        proceed: function (dir) {
+            var booldir = (dir === 1);
+            if (booldir) {
+                //ADD TO THE END OF THE WHEEL
+                this.currentRightIndex = this.realIndex(this.currentRightIndex + 1);
+
+                //ADD ELEMENT TO THE LAST
+                var cnumber = new CNumber(this.numbers[this.currentRightIndex], this.WIDTH, this.MARGIN);
+                this.registerLastNumber(cnumber);
+
+                //REMOVE FIRST ELEMENT OF THE WHEEL
+                var firstChild = this.jQueryElement.children().first();
+                this.unregisterFirstNumber();
+                firstChild.remove();
+
+                //this.lastX += this.distance;
+
+
+
+
+            } else {
+                //ADD TO THE BEGINING OF THE WHEEL
+                this.currentLeftIndex = this.realIndex(this.currentLeftIndex - 1);
+
+                //ADD ELEMENT TO THE LAST
+                var cnumber = new CNumber(this.numbers[this.currentLeftIndex], this.WIDTH, this.MARGIN);
+                this.registerFirstNumber(cnumber);
+
+                //REMOVE FIRST ELEMENT OF THE WHEEL
+                var lastChild = this.jQueryElement.children().last();
+                this.unregisterLastNumber();
+                lastChild.remove();
+
+
+                //this.lastX -= this.distance;
+            }
+        },
+        /**
+         * Executes the transformation given by the transform object.
+         * @param {Object} tm Transform object
+         * @private
+         * @returns {undefined}
+         */
+        executeTransform: function (x) {
+            var el = this.element;
+
+            if (x) {
+                var value = [
+                    'translate3d(' + x + 'px, 0, 0)'
+                ];
+                value = value.join(" ");
+                el.style.webkitTransform = value;
+                el.style.mozTransform = value;
+                el.style.transform = value;
+
+            }
+
+        },
+        /**
+         * Iterates over the wheel.
+         * @private
+         * @param {function} cb Function to be applied on all elements of the wheel.
+         * @returns {undefined}
+         */
+        iterateWheel: function (cb) {
+            var w = this.wheel;
+            var l = w.length;
+
+            for (var i = 0; i < l; i++) {
+                cb.call(this, w[i]);
+            }
+        },
+        /**
+         * This method calculates the real array index (circular) given a linear
+         * index.
+         * @param {number} i Index
+         * @private
+         * @returns {undefined}
+         */
+        realIndex: function (i) {
+            var o = this.numbers.length;
+            var dir = i < 0 ? false : true;
+            var realdistance = Math.abs(i);
+            var offset = realdistance % o;
+
+            if (realdistance < o) {
+                offset = realdistance;
+            }
+            if (dir) {
+                return offset;
+            } else {
+                return o - offset;
+            }
+
+            return i;
         }
     };
 
@@ -117,14 +464,3 @@
     return Canvas;
 });
 
-
-//PROVISONAL TOP HANDLERS
-new Canvas(document.getElementById("numbers_content"), [
-    {id: "n1", number: 20, content: "Lorem ipsun"},
-    {id: "n2", number: 12000, content: "Dorloa aoih iauhdiuha iduhi hida usdi uasdivguvu gsbnfihsbgus dbf"},
-    {id: "n3", number: 431, content: "asiu in ijn iniu asdiuhi asisu diaus ndiuasbdi udh i sdyab h9h iuehgiru bgihbdhsdb sudhfb sduhfb sudhbfsud hfbsu yfbusdyfbsu fbsu fbs"},
-    {id: "n4", number: 32, content: "Dorloa aoih iauhdiuha iduhi hida usdi uasdivguvu gsbnfihsbgus dbf"},
-    {id: "n5", number: 5, content: "ami ijasod aosid ubi i iusd iausnd asibd"},
-    {id: "n6", number: 19223213, content: "a aosid ubi i iusd iausnd asibd"},
-    {id: "n7", number: 931, content: "a aosid ubi i iusd iausnd asd ias dos iasudn aiudn iasu ndiuas dnsi dnasidu nsaid sanidu ansid asndia sdniausd "},
-]);
--- frontend/T4-in-6/js/number.js
+++ frontend/T4-in-6/js/number.js
@@ -1,15 +1,15 @@
 ;
 (function (global, factory) {
-    
+
 
     //MODULE EXPORTS
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
             //AMD MODULE
             typeof define === 'function' && define.amd ? define(factory) :
             //GLOBAL REFERENCE
-            global.CNumber = factory($);
+            global.CNumber = factory($, window);
 
-})(this, function ($) {
+})(this, function ($, window) {
     'use strict';
 
     /**
@@ -20,6 +20,31 @@
      * @constructor
      */
     var CNumber = function (params, width, margin) {
+        /**
+         * Factor to which the speed will lower to create
+         * an easing effect
+         * @type {number}
+         * @constant
+         */
+        this.EASE_DECREASE_FACTOR = 5;
+        /**
+         * Frames per second that the easing will last.
+         * @type {number}
+         * @constant
+         */
+        this.EASE_FPS = 30;
+        /**
+         * Class with the transformation ease attributes
+         * for the slowing effect.
+         * 
+         */
+        this.easeClass = "transform-numbers-ease";
+        /**
+         * DOMElement object that represents the card as a view.
+         * @protected
+         * @type {DOMElement}
+         */
+        this.jqelement = null;
         /**
          * Element object that represents the card as a view.
          * @protected
@@ -44,49 +69,175 @@
          * @type {number}
          */
         this.margin = margin;
+        /**
+         * Origin to the left
+         * @protected
+         * @type {number}
+         */
+        this.originLeft = 0;
+        /**
+         * Limit marked by the parent canvas view width.
+         * @protected
+         * @type {number}
+         */
+        this.limit = 0;
+        /**
+         * Interval number id that will handle easing out
+         * @protected
+         * @type {number}
+         */
+        this.easeInterval = null;
 
         //CALL TO PROTO METHOD init.
-        this.init();
+        this.prepare();
     };
 
     //PROTO
     CNumber.prototype = {
         /**
+         * Inits the Number Card by markin the initial position.
+         * @public
+         * @returns {undefined}
+         */
+        init: function (l) {
+            this.originLeft = l;
+            this.element.style.left = this.originLeft + "px";
+            this.limit = this.element.parentNode.offsetWidth;
+        },
+        /**
+         * Gets the current left property.
+         * @public
+         * @returns {Number}
+         */
+        getLeft:function(){
+            return this.originLeft;
+        },
+        /**
+         * Registers a new function that will receive updates on the left prop change.
+         * @public
+         * @param {function} callback
+         * @returns {undefined}
+         */
+        listen: function (callback) {
+            this.listenChange = callback;
+        },
+        /**
+         * Registers a new function that will receive updates on the left prop change.
+         * @public
+         * @param {function} callback
+         * @returns {undefined}
+         */
+        ignore: function (callback) {
+            this.listenChange = null;
+        },
+        
+        /**
+         * Marks the start of the Pan event
+         * @public
+         * @returns {undefined}
+         */
+        panStart: function () {
+            //this.jqelement.removeClass(this.easeClass);
+            //this.stopEase();
+        },
+        /**
+         * Marks the end of the Pan event
+         * @public
+         * @returns {undefined}
+         */
+        panEnd: function (evt) {
+            //this.jqelement.addClass(this.easeClass);
+            
+            this.originLeft += evt.deltaX;
+            //this.easeOut(evt);
+        },
+        /**
+         * Pans the card by a Hammer Event object properties.
+         * @param {HammerEvent} event
+         * @public
+         * @returns {undefined}
+         */
+        pan: function (event) {
+            var dist = this.originLeft + event.deltaX;
+            this.executeTransform(dist);
+        },
+        /**
          * Gets the constructed jQuery element.
          * @public
          * @returns {jQuery}
          */
-        jquery:function(){
+        jquery: function () {
             return this.jqelement;
         },
         /**
-         * Inits the basic configurations on the object.
+         * Stops the easing interval.
          * @private
          * @returns {undefined}
          */
-        init: function () {
+        stopEase:function(){
+           if(this.easeInterval){
+                window.clearInterval(this.easeInterval);
+            } 
+        },
+        /**
+         * Creates a smooth ease on the release of the panned element
+         * using the speed as main parameter.
+         * @param {HammerEvent} evt Event that was sent on "panend".
+         * @returns {undefined}
+         */
+        easeOut:function(evt){
+            var speed = evt.overallVelocityX;
+            var decrese = this.EASE_DECREASE_FACTOR * Math.abs(speed);
+            var fpsAsMillis = 1000/this.EASE_FPS;
+            
+            //console.log(evt);
+            this.easeDistance = speed*1000;
+            
+            this.easeInterval = window.setInterval((function(me,d){
+                return function(){
+                    var ed = me.easeDistance / d;
+                    console.log(ed);
+                    if(Math.abs(ed) < 0.001){
+                        me.stopEase();
+                        return;
+                    }
+                    me.pan({deltaX:ed});
+                    me.originLeft+=ed;
+                    
+                    me.easeDistance = ed;
+                };
+            })(this,decrese),fpsAsMillis);
+        },
+        /**
+         * Builds the basic structure on the object.
+         * @private
+         * @returns {undefined}
+         */
+        prepare: function () {
             var container = this.buildContainer();
             var body = this.buildBody();
             var head = this.buildHead();
-            
+
             container.append(head);
             container.append(body);
-            
+
             this.jqelement = container;
+            this.element = container[0];
         },
         /**
          * Builds the main card number container
          * @private
          * @returns {jQuery}
          */
-        buildContainer:function(){
+        buildContainer: function () {
             var el = $(document.createElement("div"));
             el.addClass("number");
-            el.attr("id",this.parameters.id);
-            el.css("width",this.width+"px");
-            el.css("margin-left",this.margin+"px");
-            el.css("margin-right",this.margin+"px");
+            el.attr("id", this.parameters.id);
+            el.css("width", this.width + "px");
+            el.css("margin-left", this.margin + "px");
+            el.css("margin-right", this.margin + "px");
             
+
             return el;
         },
         /**
@@ -94,14 +245,14 @@
          * @private
          * @returns {jQuery}
          */
-        buildHead:function(){
+        buildHead: function () {
             var el = $(document.createElement("div"));
             el.addClass("number-header");
-            
-            
+
+
             el.append(this.createImage());
             el.append(this.createNumber());
-            
+
             return el;
         },
         /**
@@ -109,16 +260,16 @@
          * @private
          * @returns {jQuery}
          */
-        createImage:function(){
+        createImage: function () {
             var image = this.parameters.image;
             var el = $(document.createElement("div"));
-            
+
             el.addClass("number-image");
-            el.css("height",(this.width/2)+"px");
-            
-            if(image){
+            el.css("height", (this.width / 2) + "px");
+
+            if (image) {
                 var img = $(document.createElement("img"));
-                img.attr("src",image);
+                img.attr("src", image);
                 el.append(img);
             }
             return el;
@@ -128,34 +279,52 @@
          * @private
          * @returns {jQuery}
          */
-        buildBody: function(){
+        buildBody: function () {
             var content = this.parameters.content;
             var el = $(document.createElement("span"));
-            
+
             el.addClass("number-content");
             el.text(content);
-            
+
             return el;
-            
+
         },
         /**
          * Builds the actual number based on the length and the box capacity
          * @private
          * @returns {jQuery}
          */
-        createNumber:function(){
+        createNumber: function () {
             var num = this.parameters.number;
             var el = $(document.createElement("span"));
             var len = num.toString().length;
-            var fontsize = (this.width/((len == 1) ? len*2: len));
-            
-            
+            var fontsize = (this.width / ((len == 1) ? len * 2 : len));
+            var height = (this.width / 2);
+
             el.addClass("number-digits");
-            el.css("font-size",fontsize+"px");
-            el.css("height",(this.width/2)+"px");
+            el.css("font-size", fontsize + "px");
+            el.css("height", height + "px");
+            el.css("line-height",height + "px");
             el.text(this.format(num));
-            
+
             return el;
+        },
+        /**
+         * Executes the transformation given by the transform object.
+         * @param {Object} tm Transform object
+         * @private
+         * @returns {undefined}
+         */
+        executeTransform: function (x) {
+            var el = this.element;
+            var lc = this.listenChange;
+
+            if (x) {
+                lc && lc.call(this,x);
+                el.style.left = x + "px";
+
+            }
+
         },
         /**
          * Builds the actual number based on the length and the box capacity
@@ -163,8 +332,8 @@
          * @private
          * @returns {string}
          */
-        format:function(num){
-             return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        format: function (num) {
+            return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         }
     };
 
